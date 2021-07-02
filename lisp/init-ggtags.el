259eba18c0f56e7de915ef6b32c69b2674658b93
Tommas Yuan <tommas1988@live.com>
2021-07-02 17:27:55 +0800

    init ggtags

--- lisp/init-ggtags.el
+++ lisp/init-ggtags.el
@@ -1,13 +1,19 @@
-;; init ggtags
+;;; init-ggtags.el --- enable ggtags -*- lexical-binding: t -*-
+;;; Commentary:
+;;; Code:
 
-(maybe-require-package 'ggtags)
-
-(with-eval-after-load 'ggtags-mode
-  (add-hook
-   'ggtags-mode-hook
-   (lambda ()
-     (local-set-key (kbd "M-*") 'pop-tag-mark)
-     (local-set-key (kbd "\C-c\C-p") `ggtags-prev-mark)
-     (local-set-key (kbd "\C-c\C-n") `ggtags-next-mark))
-   )
+(when (maybe-require-package 'ggtags)
+  (with-eval-after-load 'ggtags-mode
+    (add-hook
+     'ggtags-mode-hook
+     (lambda ()
+       (local-set-key (kbd "M-*") 'pop-tag-mark)
+       (local-set-key (kbd "\C-c\C-p") `ggtags-prev-mark)
+       (local-set-key (kbd "\C-c\C-n") `ggtags-next-mark))
+     )
+    )
   )
+
+(provide 'init-ggtags)
+
+;;; init-ggtags.el ends here
