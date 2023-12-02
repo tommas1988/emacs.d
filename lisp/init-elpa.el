bd871566d190fa609a6d6641672058963bdfaae0
tommas <tommas1988@live.com>
2023-12-02 21:57:34 +0800

    tsinghua repo

--- lisp/init-elpa.el
+++ lisp/init-elpa.el
@@ -14,9 +14,8 @@
 
 
 ;;; Standard package repositories
-
 (setq package-archives '(("gnu"   . "http://mirrors.tuna.tsinghua.edu.cn/elpa/gnu/")
-			 ("melpa" . "http://mirrors.tuna.tsinghua.edu.cn/elpa/melpa/")))
+ 			 ("melpa" . "http://mirrors.tuna.tsinghua.edu.cn/elpa/melpa/")))
 ;; Official MELPA Mirror, in case necessary.
 ;;(add-to-list 'package-archives (cons "melpa-mirror" (concat proto "://www.mirrorservice.org/sites/melpa.org/packages/")) t)
 
